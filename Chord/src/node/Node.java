ecaa8660c7e5d0927df01e088762e667ecde8c5c
matteovantadori <matteo.vantadori@mail.polimi.it>
2019-05-21 03:07:14 +0200

    Added controls on other functions

--- Chord/src/node/Node.java
+++ Chord/src/node/Node.java
@@ -41,13 +41,15 @@ public class Node {
 		}		
 		System.out.println("nodeIP\t" + nodeIP + "\nm\t" + m + "\nnodeID\t" + nodeID + "\n" + digest.toString());
 	
+		/*
 		for (BigInteger key: fingerTable.keySet()) {
             System.out.println(key + "\t" + fingerTable.get(key));
 		}
+		*/
 	}
 	
-	private Node findSuccessor(BigInteger nodeID) {
-		if(this.nodeID.compareTo(this.successor.getNodeID()) == -1)
+	public Node findSuccessor(BigInteger nodeID) {
+		if(this.nodeID.compareTo(this.successor.getNodeID()) == 1)
 			if((nodeID.compareTo(this.nodeID) == 1 && nodeID.compareTo(ringDimension) == -1) || (nodeID.compareTo(BigInteger.valueOf((long) 0)) != -1 && nodeID.compareTo(this.successor.getNodeID()) != 1))
 				return this.successor;
 		if(nodeID.compareTo(this.nodeID) == 1 && nodeID.compareTo(this.successor.getNodeID()) != 1)
@@ -55,16 +57,19 @@ public class Node {
 		return closestPrecedingNode(nodeID).findSuccessor(nodeID);
 	}
 	
-	private Node closestPrecedingNode(BigInteger nodeID) {
+	private Node closestPrecedingNode(BigInteger nodeID) { //gestire i casi con fingerTable.get(i).getNodeID() == NULL
 		for(int i = m; i > 0; i--) {
-			if(fingerTable.get(i).nodeID.compareTo(this.nodeID) == 1 && fingerTable.get(i).nodeID.compareTo(nodeID) == -1)
+			if(this.nodeID.compareTo(this.successor.getNodeID()) == 1)
+				if((fingerTable.get(i).getNodeID().compareTo(this.nodeID) == 1 && fingerTable.get(i).getNodeID().compareTo(ringDimension) == -1) || (fingerTable.get(i).getNodeID().compareTo(BigInteger.valueOf((long) 0)) != -1 && fingerTable.get(i).getNodeID().compareTo(this.successor.getNodeID()) != 1))
+					return fingerTable.get(i);
+			if(fingerTable.get(i).getNodeID().compareTo(this.nodeID) == 1 && fingerTable.get(i).getNodeID().compareTo(nodeID) == -1)
 				return fingerTable.get(i);
 		}
 		return this;
 	}	 
 	
 	public void create() {
-		this.predecessor = null;
+		this.predecessor = this; //con NULL crasha al primo stabilize
 		this.successor = this;
 	}	
 	
@@ -75,13 +80,19 @@ public class Node {
 	
 	public void stabilize() {
 		Node node = successor.getPredecessor().getSuccessor();
-		if(node.getNodeID().compareTo(this.nodeID) == 1 && node.getNodeID().compareTo(this.nodeID) == -1)
+		if(this.nodeID.compareTo(this.successor.getNodeID()) == 1)
+			if((node.getNodeID().compareTo(this.nodeID) == 1 && node.getNodeID().compareTo(ringDimension) == -1) || (node.getNodeID().compareTo(BigInteger.valueOf((long) 0)) != -1 && node.getNodeID().compareTo(this.successor.getNodeID()) == -1))
+				this.successor = node;
+		if(node.getNodeID().compareTo(this.nodeID) == 1 && node.getNodeID().compareTo(this.successor.getNodeID()) == -1)
 			this.successor = node;
 		this.successor.notify(this);
 		
 	}
 
 	public void notify(Node node) {
+		if(this.predecessor != null && this.predecessor.getNodeID().compareTo(this.nodeID) == 1)
+			if((node.getNodeID().compareTo(this.predecessor.getNodeID()) == 1 && node.getNodeID().compareTo(ringDimension) == -1) || (node.getNodeID().compareTo(BigInteger.valueOf((long) 0)) != -1 && node.getNodeID().compareTo(this.nodeID) == -1))
+				this.predecessor = node;
 		if(this.predecessor == null || (node.getNodeID().compareTo(this.predecessor.getNodeID()) == 1 && node.getNodeID().compareTo(this.nodeID) == -1))
 			this.predecessor = node;
 	}
@@ -129,11 +140,15 @@ public class Node {
 
 	public static void main (String[] args) throws NoSuchAlgorithmException, IOException {
 		Node n1 = new Node();
-		//Node n2 = new Node();
+		Node n2 = new Node();
 		n1.create();
-		//Listener l = new Listener();//InetAddress.getLocalHost()
-		//new Thread(l).start();
-		//n2.join(n1);
-		System.out.println(n1.successor.getNodeID());
+		n2.join(n1);
+		n2.setNodeID(n2.getNodeID().add(BigInteger.valueOf((long) 1)));
+		System.out.println("New N2 ID: " + n2.getNodeID());
+		n1.stabilize();
+		n2.stabilize();
+		System.out.println("N1 ID: " + n1.getNodeID() + "\nN1 successor: " + n1.successor.getNodeID() + "\nN1 predecessor: " + n1.predecessor.getNodeID());
+		System.out.println("N2 ID: " + n2.getNodeID() + "\nN2 successor: " + n2.successor.getNodeID() + "\nN2 predecessor: " + n2.predecessor.getNodeID());	
+		n1.stabilize();
 	}
 }
